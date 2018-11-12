b69d66cd7c9f7f89516c96ef40ca7e50491e26d5
Pessi94 <Pessi.pe@gmail.com>
2018-11-12 12:01:35 +0100

    Komentarz

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         JaroWinkler jaroWinkler = new JaroWinkler();
 
-        System.out.println(jaroWinkler.calculateSimilarity("DWAYNE", "DUANE"));
+        System.out.println(jaroWinkler.calculateSimilarity("DWAYNE", "DUANE")); //komentarz
         System.out.println(jaroWinkler.calculateSimilarity("MARTHA ", "MARHTA "));
     }
 }
