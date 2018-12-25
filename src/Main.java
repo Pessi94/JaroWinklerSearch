5aeab8e08ede40d5bc988299e5a4ee21a3756f4c
Pessi94 <Pessi.pe@gmail.com>
2018-12-25 11:30:27 +0100

    Wczytywanie tekstu z pliku

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,15 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    public static final String TEXT =
-            "Na tapczanie siedzi leń, " +
-            "Nic nie robi cały dzień. " +
-            "\"O, wypraszam to sobie! " +
-            "Jak to? Ja nic nie robię? " +
-            "A kto siedzi na tapczanie? " +
-            "A kto zjadł pierwsze śniadanie? " +
-            "A kto dzisiaj pluł i łapał? " +
-            "A kto się w głowę podrapał? " +
-            "A kto dziś zgubił kalosze? " +
-            "O - o! Proszę!\" " +
-            "Na tapczanie siedzi leń, " +
-            "Nic nie robi cały dzień. " +
-            "\"Przepraszam! A tranu nie piłem? " +
-            "A uszu dzisiaj nie myłem? " +
-            "A nie urwałem guzika? " +
-            "A nie pokazałem języka? " +
-            "A nie chodziłem się strzyc? " +
-            "To wszystko nazywa się nic?\" " +
-            "Na tapczanie siedzi leń, " +
-            "Nic nie robi cały dzień. " +
-            "Nie poszedł do szkoły, bo mu się nie chciało, " +
-            "Nie odrobił lekcji, bo czasu miał za mało, " +
-            "Nie zasznurował trzewików, bo nie miał ochoty, " +
-            "Nie powiedział \"dzień dobry\", bo z tym za dużo roboty, " +
-            "Nie napoił Azorka, bo za daleko jest woda, " +
-            "Nie nakarmił kanarka, bo czasu mu było szkoda. " +
-            "Miał zjeść kolację - tylko ustami mlasnął, " +
-            "Miał położyć się - nie zdążył - zasnął. " +
-            "Śniło mu się, że nad czymś ogromnie się trudził. " +
-            "Tak zmęczył się tym snem, że się obudził. ";
+public class Main {
 
     public static final String WORD_TO_FIND = "na";
 
     public static void main(String[] args) {
         JaroWinkler jaroWinkler = new JaroWinkler();
-        String[] words = TEXT.toLowerCase().split(" ");
+        String[] words = readWords();
 
         writeWords(words);
 
@@ -59,6 +32,29 @@ public class Main {
                 System.out.println("Index znalezionego słowa to: " + i);
             }
         }
+    }
+
+    private static String[] readWords(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("words.txt"));
+            ArrayList<String> words = new ArrayList<>();
+            String word;
+
+            while (true) {
+                word = reader.readLine();
+                if (word == null) {
+                    break;
+                }
+                words.add(word);
+            }
+
+            return words.toArray(new String[0]);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 }
